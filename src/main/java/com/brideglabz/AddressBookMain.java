6580a96d730105129b4835d7c7be5f8cce9a021a
shwetap29 <shwetaprasad48@gmail.com>
2021-05-28 18:20:09 +0530

    ADDED: created contact details

--- src/main/java/com/brideglabz/AddressBookMain.java
+++ src/main/java/com/brideglabz/AddressBookMain.java
@@ -1,5 +1,47 @@
 package com.brideglabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookMain {
-    Pub
+    public static void main(String[] args) {
+        List<String> person = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Address Book System");
+
+        System.out.println("Enter First name: ");
+        String fname = input.nextLine();
+
+        System.out.println("Enter Last name: ");
+        String lname = input.nextLine();
+
+        System.out.println("Enter First Address: ");
+        String address = input.nextLine();
+
+        System.out.println("Enter City: ");
+        String city = input.nextLine();
+
+        System.out.println("Enter State: ");
+        String state = input.nextLine();
+
+        System.out.println("Enter Zip: ");
+        String zip = input.nextLine();
+
+        System.out.println("Enter Phone number: ");
+        String phone = input.nextLine();
+
+        person.add(fname);
+        person.add(lname);
+        person.add(address);
+        person.add(city);
+        person.add(state);
+        person.add(zip);
+        person.add(phone);
+
+        for (String str : person){
+            System.out.println(str);
+        }
+    }
 }
